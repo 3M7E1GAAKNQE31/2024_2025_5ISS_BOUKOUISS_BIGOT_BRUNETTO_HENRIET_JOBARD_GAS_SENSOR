934f6b688b458d7b2b69e970b5f7f7c9c938b4b0
3M7E1GAAKNQE31 <151733903+3M7E1GAAKNQE31@users.noreply.github.com>
2024-12-11 10:35:17 +0100

    Add files via upload

---
+++ envoie_donnees_chirpstack/envoie_donnees_chirpstack.ino
@@ -0,0 +1,60 @@
+#include <TheThingsNetwork.h>
+#include <SoftwareSerial.h>
+
+#define loraSerial mySerial
+#define debugSerial Serial
+
+// Replace REPLACE_ME with TTN_FP_EU868 or TTN_FP_US915
+#define freqPlan TTN_FP_EU868
+
+
+const byte txPin = 11;
+const byte rxPin = 10;
+const int gasSensorPin = 0;
+
+SoftwareSerial mySerial (rxPin,txPin);
+TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
+
+
+const char *appEui = "0004A30B002159CA";
+const char *appKey = "8d1630b9b6db6270fa1ae3268e610842";
+int val = 0;
+
+void setup()
+{
+  loraSerial.begin(57600);
+  debugSerial.begin(9600);
+  ttn.join(appEui,appKey);
+  //Serial.begin(9600);
+  debugSerial.print("test serial");
+}
+
+void loop()
+{
+//  debugSerial.println("Device Information");
+//  debugSerial.println();
+//  ttn.showStatus();
+//  debugSerial.println();
+//  debugSerial.println("Use the EUI to register the device for OTAA");
+//  debugSerial.println("-------------------------------------------");
+//  debugSerial.println();
+//  byte data[2];
+//  data[0] = 0x11;
+//  data[1] = 0x10;
+//  ttn.sendBytes(data,sizeof(data));
+//  debugSerial.println("Byte envoyé");
+  val = analogRead(gasSensorPin);
+  debugSerial.print("valeur du capteur:");
+  debugSerial.println(val);
+
+  byte payload[2];
+  payload[0] = (val >> 8) & 0xFF;
+  payload[1] = val & 0xFF;
+  ttn.sendBytes(payload, sizeof(payload));
+
+  debugSerial.print("print payload");
+  debugSerial.print(payload[0]);
+  debugSerial.print(payload[0]);
+  delay(1000);
+  
+  }
